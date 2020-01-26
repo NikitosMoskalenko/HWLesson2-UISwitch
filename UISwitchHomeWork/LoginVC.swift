046693b02a7d5234606289d5a4910f2e606aaaee
Moskalenko Nikita <n.moskalneko1996@yahoo.com>
2020-01-27 02:47:05 +0300

    With repairing

--- UISwitchHomeWork/LoginVC.swift
+++ UISwitchHomeWork/LoginVC.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+final class LoginVC: UIViewController {
     
 //  MARK: - Constants
-    private let lableUSD = UILabel()
+    private let labelUSD = UILabel()
     private let textFieldUSD = UITextField()
-    private let lableEUR = UILabel()
+    private let labelEUR = UILabel()
     private let textFieldEUR = UITextField()
-    private let lableRUR = UILabel()
+    private let labelRUB = UILabel()
     private let textFieldRUR = UITextField()
     private let convertButton = UIButton()
     private let resultTextField = UITextField()
@@ -24,48 +24,84 @@ class LoginVC: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.green
+
+    }
         
-//      add USD Lable
-        view.addSubview(lableUSD)
-        lableUSD.text = "USD:"
-        lableUSD.frame = CGRect(x: 120, y: 200, width: 50, height: 25)
+     enum typeOfCurrency {
+         case USD
+         case EUR
+         case RUB
+         
+         var foriengCurrency: String {
+             switch self {
+             case .USD:
+                 return "USD:"
+             case .EUR:
+                 return "EUR:"
+             case .RUB:
+                 return "RUB:"
+             }
+         }
+     }
+    
+//    MARK: - Provate methods
+    
+//      add USD Lable & TextField
+    private func addUSD() {
+        view.addSubview(labelUSD)
+        labelUSD.text = typeOfCurrency.USD.foriengCurrency
+        labelUSD.frame = CGRect(x: 120, y: 200, width: 50, height: 25)
         
-//      add USD TextField
         view.addSubview(textFieldUSD)
         textFieldUSD.frame = CGRect(x: 120, y: 230, width: 180, height: 25)
         textFieldUSD.backgroundColor = .white
+    }
+ 
+//      add EUR label & TextField
+    private func addEUR() {
+        view.addSubview(labelEUR)
+        labelEUR.text = typeOfCurrency.EUR.foriengCurrency
+        labelEUR.frame = CGRect(x: 120, y: 270, width: 50, height: 25)
         
-//      add EUR Lable
-        view.addSubview(lableEUR)
-        lableEUR.text = "EUR:"
-        lableEUR.frame = CGRect(x: 120, y: 270, width: 50, height: 25)
-        
-//      add EUR TextField
         view.addSubview(textFieldEUR)
         textFieldEUR.frame = CGRect(x: 120, y: 300, width: 180, height: 25)
         textFieldEUR.backgroundColor = .white
+    }
+    
+//      add RUB Label & TextField
+    private func addRUB() {
+        view.addSubview(labelRUB)
+        labelRUB.text = typeOfCurrency.RUB.foriengCurrency
+        labelRUB.frame = CGRect(x: 120, y: 340, width: 50, height: 25)
         
-//      add RUR Lable
-        view.addSubview(lableRUR)
-        lableRUR.text = "RUB:"
-        lableRUR.frame = CGRect(x: 120, y: 340, width: 50, height: 25)
-        
-//      add RUR TextField
         view.addSubview(textFieldRUR)
         textFieldRUR.frame = CGRect(x: 120, y: 370, width: 180, height: 25)
         textFieldRUR.backgroundColor = .white
-        
-//      add convert Button
+    }
+    
+// add Button to convert currency
+    private func addConvertButton() {
         view.addSubview(convertButton)
         convertButton.frame = CGRect(x: 160, y: 410, width: 100, height: 30)
         convertButton.setTitle("Convert", for: .normal)
         convertButton.backgroundColor = .red
         convertButton.addTarget(self, action: #selector(USDcurrencyExchange), for: .touchUpInside)
-        
-//      add result textField
+    }
+    
+//   add resulting text field
+    private func addResultTextField() {
         view.addSubview(resultTextField)
         resultTextField.backgroundColor = .white
         resultTextField.frame = CGRect(x: 120, y: 450, width: 180, height: 25)
+    }
+    
+//   add allert controller "Pleas enter the value"
+    private func createdAlertController() {
+        let alertController = UIAlertController(title: "Wraning", message: "Pleas enter value", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true)
     }
     
 //  MARK: - Public methods
@@ -73,19 +109,14 @@ class LoginVC: UIViewController {
     @objc func USDcurrencyExchange() {
         if let usdTextField = textFieldUSD.text, let eurTextField = textFieldEUR.text, let rubTextField = textFieldRUR.text {
             
-            if usdTextField.count != 0 {
-                resultTextField.text = "\((Double(usdTextField)!) * 2.17)"
-            } else if eurTextField.count != 0 {
-                resultTextField.text = "\((Double(eurTextField)!) * 2.33)"
-            } else if rubTextField.count != 0 {
-                resultTextField.text = "\((Double(rubTextField)!) * 0.034)"
+            if usdTextField.count != 0, let doubleUSD = Double(usdTextField) {
+                resultTextField.text = "\(doubleUSD * 2.17)"
+            } else if eurTextField.count != 0, let doubleEUR = Double(eurTextField) {
+                resultTextField.text = "\(doubleEUR * 2.33)"
+            } else if rubTextField.count != 0, let doublRUB = Double(rubTextField) {
+                resultTextField.text = "\(doublRUB * 0.034)"
             } else {
-                let alertController = UIAlertController(title: "Wraning", message: "Pleas enter value", preferredStyle: .alert)
-                let alertAction = UIAlertAction(title: "Ok", style: .cancel) { (action) in
-                }
-                
-                alertController.addAction(alertAction)
-                self.present(alertController, animated: true)
+                createdAlertController()
             }
         }
     }
--- UISwitchHomeWork/ViewController.swift
+++ UISwitchHomeWork/ViewController.swift
@@ -8,45 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+final class ViewController: UIViewController, UITextFieldDelegate {
 
 //  MARK: - Constants
 
     private let namedTextField = UITextField()
-    private let loginLable = UILabel()
+    private let loginLabel = UILabel()
     private let passwordTextField = UITextField()
     private let passwordLable = UILabel()
     private let loginButton = UIButton()
-    private let acceptLable = UILabel()
+    private let acceptLabel = UILabel()
     private let acceptSwitch = UISwitch()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.green
-        
-//      add loginLable
-        view.addSubview(loginLable)
-        loginLable.text = "Login:"
-        loginLable.textColor = .black
-        loginLable.frame = CGRect(x: 120, y: 325, width: 50, height: 25)
-        
-        
-//      add namedTexteField
+   
+    }
+    
+//   MARK: - Private methods
+    
+//   created loginLabel
+    private func addLoginLabel() {
+        view.addSubview(loginLabel)
+        loginLabel.text = "Login:"
+        loginLabel.textColor = .black
+        loginLabel.frame = CGRect(x: 120, y: 325, width: 50, height: 25)
+    }
+    
+//  created namedTextField
+    private func addNamedTextField() {
         view.addSubview(namedTextField)
         namedTextField.frame = CGRect(x: 120, y: 350, width: 180, height: 25)
         namedTextField.backgroundColor = UIColor.white
         namedTextField.placeholder = "Enter your login..."
         namedTextField.textAlignment = .center
         namedTextField.textColor = UIColor.black
-        
-//      add passwordLable
+    }
+    
+//   created password label
+    private func addPasswordLabel() {
         view.addSubview(passwordLable)
         passwordLable.text = "Password:"
         passwordLable.textColor = .black
         passwordLable.frame = CGRect(x: 120, y: 390, width: 100, height: 25)
-        
-//      add passwordTextField
+    }
+    
+//   created password text field
+    private func addPasswordTextField() {
         view.addSubview(passwordTextField)
         passwordTextField.frame = CGRect(x: 120, y: 415, width: 180, height: 25)
         passwordTextField.backgroundColor = UIColor.white
@@ -54,48 +64,54 @@ class ViewController: UIViewController, UITextFieldDelegate {
         passwordTextField.textAlignment = .center
         passwordTextField.textColor = UIColor.black
         passwordTextField.isSecureTextEntry = true
-        
-//      add loginButton
+    }
+    
+//   created login button
+    private func addLoginButton() {
         view.addSubview(loginButton)
         loginButton.setTitle("Login", for: .normal)
         loginButton.frame = CGRect(x: 160, y: 445, width: 100, height: 30)
         loginButton.backgroundColor = .red
         loginButton.addTarget(self, action: #selector(showSecondViewController), for: .touchUpInside)
-        
-//      add Accept Lable
-        view.addSubview(acceptLable)
-        acceptLable.text = "I'm alredy reading and agree with rule..."
-        acceptLable.frame = CGRect(x: 140, y: 485, width: 200, height: 25)
-        acceptLable.textColor = .red
-        
-//      add Switch
+    }
+    
+//   created accept label
+    private func addAcceptLabel() {
+        view.addSubview(acceptLabel)
+        acceptLabel.text = "I'm alredy reading and agree with rule..."
+        acceptLabel.frame = CGRect(x: 140, y: 485, width: 200, height: 25)
+        acceptLabel.textColor = .red
+    }
+    
+//   created switch
+    private func addSwitchElement() {
         view.addSubview(acceptSwitch)
         acceptSwitch.frame = CGRect(x: 80, y: 485, width: 0, height: 0)
         acceptSwitch.addTarget(self, action: #selector(showSecondViewController), for: .valueChanged)
-        
     }
     
-//   MARK: - Private methods
-    
+//    crated alert controllers
+    private func createdAlertControllers(titleAction: String, titleAC: String, message: String, styleAction: UIAlertAction.Style ,styleAC: UIAlertController.Style) {
+        let newAlerController = UIAlertController(title: titleAC, message: message, preferredStyle: styleAC)
+        let newAlertAction = UIAlertAction(title: titleAction, style: styleAction, handler: nil)
+        
+        newAlerController.addAction(newAlertAction)
+        present(newAlerController, animated: true)
+    }
+
+// the condition for crossing to second VC
     @objc private  func showSecondViewController() {
+//      optinal binding
         if let loginCount = namedTextField.text?.count, let paswordCount = passwordTextField.text?.count {
               if loginCount == 0, paswordCount == 0 {
-                  let wraningMessage = UIAlertController(title: "Wraning", message: "Your password or email isn't corrected", preferredStyle: .actionSheet)
-                  let action = UIAlertAction(title: "Ok", style: .cancel) { (action) in
-                  }
-                  
-                  wraningMessage.addAction(action)
-                  present(wraningMessage, animated: true)
+//                add alert
+                createdAlertControllers(titleAction: "Ok", titleAC: "Wraning", message: "Your password or email isn't corrected", styleAction: .cancel, styleAC: .alert)
+                
               } else if acceptSwitch.isOn {
                   let secondVC = LoginVC()
                   present(secondVC, animated: true)
               } else {
-                  let alertController = UIAlertController(title: "Wraning", message: "Pleas accept the rules", preferredStyle: .alert)
-                  let alertAction = UIAlertAction(title: "OK", style: .cancel) { (alertAction) in
-                  }
-                  
-                  alertController.addAction(alertAction)
-                  self.present(alertController, animated: true)
+                createdAlertControllers(titleAction: "Ok", titleAC: "Wraning", message: "Pleas accept the rules", styleAction: .cancel, styleAC: .alert)
               }
           }
       }
